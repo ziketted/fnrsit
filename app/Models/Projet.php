4c8feea74655ff652c15b204774be6b4f633d1a9
jospin <jospinmpoyi92@gmail.com>
2024-09-19 13:08:30 +0100

    creation modes : actionnaire, entrepreneur, principaleDepense, projet, secteur,user

--- app/Models/Projet.php
+++ app/Models/Projet.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Projet extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $fillable= [
+        'user_id',
+        'titre',
+        'proprieteur',
+        'budget',
+        'secteur',
+        'debut',
+        'fin',
+    ];
 }
