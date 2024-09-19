1de8a3702d7ecb0eef969efe778a12c20aa2d7ae
jospinmpoyi <113398709+jospinmpoyi@users.noreply.github.com>
2024-09-19 16:02:24 +0100

    creation formulaire actionnaire, entrepreneur, principaleDepense, projet, secteur, user

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,16 +7,17 @@
     <div class="app-content pt-3 p-md-3 p-lg-4">
         <div class="container-xl">
 
-            <h1 class="app-page-title">Tableau de bord</h1>
+            <h1 class="app-page-title"> Fonds
+            national de la recherche scientifique et de l’innovation technologique, « FNRSIT» en sigle</h1>
 
             <div class="app-card alert alert-dismissible shadow-sm mb-4 border-left-decoration" role="alert">
                 <div class="inner">
                     <div class="app-card-body p-3 p-lg-4">
-                        <h3 class="mb-3">Welcome, developer!</h3>
+                        <h3 class="mb-3">Bienvenue dans notre application bussnes plan</h3>
                         <div class="row gx-5 gy-3">
                             <div class="col-12 col-lg-9">
 
-                                <div>Portal is a free Bootstrap 5 admin dashboard template. The design is simple, clean and modular so it's a great base for building any modern web app.</div>
+                                <div>Le but du business plan est de rassurer les partenaires de l'entreprise (banque, investisseurs, etc.), de mettre en valeur l'entreprise pour convaincre des investisseurs et recruter de nouveaux employés, et de cadrer l'activité pour être le plus réaliste possible. Il ne faut pas confondre le business plan avec l'étude de marché.</div>
                             </div><!--//col-->
                             <div class="col-12 col-lg-3">
                                 <a class="btn app-btn-primary" href="https://themes.3rdwavemedia.com/bootstrap-templates/admin-dashboard/portal-free-bootstrap-admin-dashboard-template-for-developers/"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-arrow-down me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
